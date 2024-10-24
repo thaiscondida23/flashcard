8e508a41af97e6e20240dc90c2b6b10c3e8f5564
lula meu amor #13 <141644195+thaiscondida23@users.noreply.github.com>
2024-10-24 07:30:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function criaCarta(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+    cartao.className = "cartao"
 
 cartao.innerHTML = ` 
 <div class="cartao__conteudo">
