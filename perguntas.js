c0a4bf0c5bed231c48402c18e6a195ba2d8fc7b0
lula meu amor #13 <141644195+thaiscondida23@users.noreply.github.com>
2024-10-24 07:54:48 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,31 +1,31 @@
-criaCartao{
+criaCartao(
     'Livro:Caraval', 
     'Quem é o Lenda?',
     'Dante',
-}
-criaCartao{
+)
+criaCartao(
     'Livro:Caraval',
     'Qual o apelido da personagem principal?',
     'Carmin',
-}
-criaCartao{
+)
+criaCartao(
     'Filme: Batman: cavaleiro das trevas',
     'Qual o oscar ganho desse filme?',
     ' um Oscar na categoria de atuação: Heath Ledger recebeu o prêmio postumamente',
-}
-criaCartao{
+)
+criaCartao(
     'Batman',
     'Quem foi o segundo robin?',
     'Jason Todd',
-}
-criaCartao{
+)
+criaCartao(
     'Livro:Era uma vez um coração partido?',
     'Qual era a maldição do Jacks?',
     'Quem ele beijar morre',
-}
-criaCartao{
+)
+criaCartao(
     'filme shrek',
     'quantos filhos ele tem?',
     'ele tem 3 filhos',
-}
-'
+)
+
